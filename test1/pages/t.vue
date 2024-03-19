6b048480f3ab46946d84b01d7f6ec2c725373c13
silgon <silgon3200@gmail.com>
2024-03-19 23:43:57 +0100

    initial commit

---
+++ test1/pages/t.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+ const route = useRoute()
+ console.log(route.params)
+</script>
+<template>
+  <h1>{{ route.params.a }}</h1>
+  <nuxt-link :to="{params:{a:'hello'}}">hello</nuxt-link>
+  <br/>
+  <nuxt-link :to="{params:{a:'there'}}">there</nuxt-link>
+</template>
